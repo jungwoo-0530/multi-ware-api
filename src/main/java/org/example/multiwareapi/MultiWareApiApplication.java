d0b98fd221426c007f82b6cc0f22c23bce31a832
jungwoo-0530 <am7227@ajou.ac.kr>
2024-04-11 10:12:43 +0900

    init

---
+++ src/main/java/org/example/multiwareapi/MultiWareApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.multiwareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiWareApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiWareApiApplication.class, args);
+    }
+
+}
